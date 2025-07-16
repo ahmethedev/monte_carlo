18ef92c463ae09f38f31aaae279374164e0e7661
ahmethedev <ahmethedev@gmail.com>
2025-07-16 23:27:54 +0300

    fixed signout

--- monte-client/src/components/Header.tsx
+++ monte-client/src/components/Header.tsx
@@ -29,9 +29,9 @@ export const Header = () => {
   return (
     <header className="bg-gray-900/50 backdrop-blur-md z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between h-16">
-          
-          
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+          </div>
           {user && (
             <div className="flex items-center">
               <button
